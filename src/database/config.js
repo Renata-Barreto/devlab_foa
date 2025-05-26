93162c47f7006c14f7d1e1d64c0420edbd32393b
dicolab7 <dico.colab7@gmail.com>
2025-05-26 18:40:10 -0300

    Add files via upload

--- src/database/config.js
+++ src/database/config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 console.log('Variáveis de ambiente:', {
+  DATABASE_URL: process.env.DATABASE_URL ? '******' : 'undefined',
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD ? '******' : 'undefined',
@@ -17,18 +18,22 @@ console.log('Variáveis de ambiente:', {
   DB_SSL: process.env.DB_SSL,
 });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: process.env.DB_SSL === 'true' ? { ssl: { require: true, rejectUnauthorized: false } } : {},
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL || {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  dialectOptions: process.env.DB_SSL === 'true' ? {
+    ssl: { require: true, rejectUnauthorized: false }
+  } : {},
+}, {
+  logging: false,
+  dialectOptions: process.env.DATABASE_URL ? {
+    ssl: { require: true, rejectUnauthorized: false }
+  } : {}
+});
 
 sequelize.authenticate()
   .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso.'))
